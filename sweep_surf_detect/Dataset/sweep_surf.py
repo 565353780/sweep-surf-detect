a756d99b77262174bfd29918c55d8b9ecb52b776
chLi <565353780@qq.com>
2025-09-21 15:44:42 +0800

    remove unused code

--- sweep_surf_detect/Dataset/sweep_surf.py
+++ sweep_surf_detect/Dataset/sweep_surf.py
@@ -5,34 +5,6 @@ from torch.utils.data import Dataset
 from sweep_surf_detect.Data.sweep_surf import SweepSurf
 
 
-def test():
-    num_curve_ctrlpts = 3
-    is_curve_closed = False
-    curve_t_samples = 100
-    num_plane_curve_ctrlpts = 3
-    is_plane_curve_closed = False
-    plane_curve_t_samples = 100
-
-    curve_ctrlpts = np.random.randn(num_curve_ctrlpts, 3)
-
-    plane_curve_ctrlpts = np.random.randn(num_plane_curve_ctrlpts, 2)
-
-    sweep_surf = SweepSurf(
-        curve_ctrlpts,
-        is_curve_closed,
-        plane_curve_ctrlpts,
-        is_plane_curve_closed,
-    )
-
-    visualize_sweep_surface(
-        sweep_surf,
-        curve_t_samples,
-        plane_curve_t_samples,
-    )
-
-    return True
-
-
 class SweepSurfDataset(Dataset):
     def __init__(
         self,
